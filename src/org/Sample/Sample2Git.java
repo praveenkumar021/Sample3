56c89c0bf7777a5a58a46f8764f5636ab9f58045
praveenkumar021 <pravee.nku04@gmail.com>
2021-07-17 23:18:25 +0530

    third

--- src/org/Sample/Sample2Git.java
+++ src/org/Sample/Sample2Git.java
@@ -11,7 +11,7 @@ public static void main(String[] args) {
 	System.out.println("rt");
 	System.out.println("mk");
 	
-<<<<<<< HEAD
+
 	System.out.println("saravanan");
 	System.out.println("kumar");
 	System.out.println("velmurugan");
@@ -19,13 +19,12 @@ public static void main(String[] args) {
 	System.out.println("pk");
 	System.out.println("rt");
 	System.out.println("mk");
-=======
+
 	System.out.println("pk");
 	System.out.println("rt");
 	System.out.println("mk");
 	
-	
->>>>>>> 03591127ad9c8059e378a1578699b3c6aeabca0e
+
 }
 
 
