8f76381bc0f006624d055d527d044941874792b8
praveenkumar021 <pravee.nku04@gmail.com>
2021-07-17 23:14:47 +0530

    second2

--- src/org/Sample/Sample2Git.java
+++ src/org/Sample/Sample2Git.java
@@ -10,6 +10,14 @@ public static void main(String[] args) {
 	System.out.println("pk");
 	System.out.println("rt");
 	System.out.println("mk");
+	
+	System.out.println("saravanan");
+	System.out.println("kumar");
+	System.out.println("velmurugan");
+	
+	System.out.println("pk");
+	System.out.println("rt");
+	System.out.println("mk");
 }
 
 
