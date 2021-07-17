8ff24c2e6c1240588d9d296ae0a2bf97a2ce83b6
praveenkumar021 <pravee.nku04@gmail.com>
2021-07-17 10:43:52 +0530

    First Commit

---
+++ src/org/Sample/Sample2Git.java
@@ -0,0 +1,12 @@
+package org.Sample;
+
+public class Sample2Git {
+public static void main(String[] args) {
+
+	System.out.println("saravanan");
+	System.out.println("kumar");
+	System.out.println("velmurugan");
+}
+
+
+}
